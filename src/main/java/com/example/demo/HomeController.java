e0f337277bf0410fd54c891bc3a06d7061303a08
GBTC440017UR <GBTC440017UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-03 14:37:21 -0500

    402 Done

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+/*401*/
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    /*402*/
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
